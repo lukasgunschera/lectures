ffd48b1fddd47715af0d0da8f50d0695c76da1b0
lukasgunschera <l.gunschera@outlook.com>
2025-10-29 12:05:46 +0000

    formatting

--- 2024_robust_statistics/2024_rs_demonstrations.R
+++ 2024_robust_statistics/2024_rs_demonstrations.R
@@ -62,7 +62,7 @@ ggsave(gg_p1_distr,
 
 ## Effect 2 -------------------------------------------------------------------------------------------------------------- ##
 # increase sample size
-nsample <- 127      # sample size of both groups
+nsample <- 127      # increase sample size of both groups
 
 p2 <- numeric(nexp) # store all simulated *p*-values
 
@@ -97,8 +97,47 @@ ggsave(gg_p2_distr,
 )
 
 ## Effect 3 -------------------------------------------------------------------------------------------------------------- ##
+# decrease standard deviation
+sd1 <- 10          # standard deviation of group 1
+sd2 <- 10          # standard deviation of group 2
+
+p2 <- numeric(nexp) # store all simulated *p*-values
+
+for (i in 1:nexp) { # for each simulated experiment
+  x2 <- rnorm(n = nsample, mean = m1, sd = sd1) # Simulate data
+  y2 <- rnorm(n = nsample, mean = m2, sd = sd2) # Simulate data
+  p2[i] <- t.test(x2, y2)$p.value # store the *p*-value
+}
+
+# computer power (probability of rejecting null when it is false)
+round((sum(p2 < 0.05) / nexp), 2)
+
+gg_p2_distr <- p2 %>%
+  data.frame(pvals = p2) %>%
+  ggplot(., aes(x = pvals)) +
+  geom_histogram(breaks = seq(0, 1, by = 0.05), color = "#2E2E2E", alpha = 0.7, aes(fill = pvals < 0.05)) +
+  scale_y_continuous(limits = c(0, nexp)) +
+  scale_x_continuous(breaks = seq(0, 1, .1)) +
+  labs(x = "p-value", y = "Number of p-values") +
+  plot_theme +
+  aspect_ratio_balanced +
+  scale_fill_viridis_d(direction = -1) +
+  geom_hline(yintercept = 5000, color = "red", linetype = "dashed", size = .25) +
+  annotate(geom = "segment", x = 0, xend = 1, y = -Inf, yend = -Inf, colour = "#2E2E2E", lwd = 0.75) +
+  annotate(geom = "segment", x = -Inf, xend = -Inf, y = 0, yend = 100000, colour = "#2E2E2E", lwd = 0.75)
+
+print(gg_p2_distr)
+
+ggsave(gg_p2_distr,
+       path = here::here("2024_robust_statistics", "images"), filename = "rs_pvalue_p2_distr.png",
+       dpi = 1200, device = "png"
+)
+
+## Effect 4 -------------------------------------------------------------------------------------------------------------- ##
 # change group means to be equivalent
-m2 <- 100      # mean of group 2
+m2 <- 100          # mean of group 2
+sd1 <- 15          # standard deviation of group 1
+sd2 <- 15          # standard deviation of group 2
 
 pnull <- numeric(nexp) # store all simulated *p*-values
 
@@ -123,7 +162,7 @@ gg_pnull_distr <- pnull %>%
   annotate(geom = "segment", x = -Inf, xend = -Inf, y = 0, yend = 100000, colour = "#2E2E2E", lwd = 0.75)
 
 # computer power (probability of rejecting null when it is false)
-round((sum(pnull < 0.05) / 100000), 2)
+round((sum(pnull < 0.05) / 100000), 2) # false positive (type 1 error)
 
 print(gg_pnull_distr)
 
@@ -143,7 +182,7 @@ shotData <- c(1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 
               0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1,
               1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0)
 
-x = seq(.001, .999, .001) ##Set up for creating the distributions
+x = seq(.001, .999, .001) #Set up for creating the distributions
 y2 = dbeta(x, 1 + 58, 1 + 42) # data for likelihood curve, plotted as the posterior from a be
 
 # probability of success (three pointer)
@@ -158,7 +197,7 @@ plot(seq(.001, .999, .001), dbeta(x, 1 + 58, 1 + 42),
 axis(1, at = seq(0, 1, .2)) # adds custom x axis
 axis(2, las = 1) # custom y axis
 
-## Plot function -------------------------------------------------------------------------------------------------------------- ##
+## Plot function --------------------------------------------------------------------------------------------------------- ##
 # @ PS = prior successes (input to beta distribution)
 # @ PF = prior failures (input to beta distribution)
 # @ k = number of observed successes in the data
@@ -189,12 +228,8 @@ plot.beta(4, 9, ymax = 3.2, main = "Informed Prior, Beta(4,9)")
 # miss or make next one?
 plot.beta(4, 9, 1, 1, ymax = 3.2, main = "Informed Prior, Beta(4,9)")
 
-
-
 plot.beta(4, 9, 13, 25, main = "Beta(4,9) to Beta(17,21)", ymax = 10)
 plot.beta(17, 21, 12, 25, ymax = 10, main = "Beta(17,21) to Beta(29,34)")
 plot.beta(29, 34, 14, 25, ymax = 10, main = "Beta(29,34) to Beta(43,45)")
 plot.beta(43, 45, 19, 25, ymax = 10, main = "Beta(43,45) to Beta(62,51)")
 plot.beta(4, 9, 58, 100, ymax = 10, main = "Beta(4,9) to Beta(62,51)") # after 100 throws
-
-
